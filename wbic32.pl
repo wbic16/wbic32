93d81386c05c176fb462c14092255da9a45a2bf4
Will Bickford <wbic16@gmail.com>
2014-11-21 08:11:35 -0600

    Cleanup and range testing
    Added GetCriticalRating(), GetPriceRating(), and GetRandomHoldMessage()
    while re-enabling range-finding for my info only atm.

--- wbic32.pl
+++ wbic32.pl
@@ -86,6 +86,10 @@ sub LoadBitcoinPriceData
 			$config->param("last_price", $price);
 		}
 	}
+	if (!IsActive())
+	{
+		$btc_date = $parms{'last_checked'};
+	}
 	# Hack to keep quotes
 	$config->param("last_checked", "\"" . $btc_date . "\"");
 
@@ -124,15 +128,12 @@ sub CalculateNextEMA
 	my $factor = $denominator - 1;
 
 	my $ema = RoundToTwoDecimals(($last_average * $factor + $price) / $denominator);
-	say "Next EMA: $ema";
 	return $ema;
 }
 
 sub GetBitcoinPriceRating
 {
-	my @hold_messages = ('Ho Ho Ho Hold!', 'HODLing like a boss', 'Just keep keep holding, just keep holding ... what do we do we HODL!');
-	my $luck = int(rand(3));
-	my $rating = $hold_messages[$luck];
+	my $rating = GetRandomHoldMessage();
 	my $price = GetBitcoinAveragePrice();
 	my $last_average = $parms{'last_average'};	
 	my $next_average = CalculateNextEMA($last_average, $price, 60);
@@ -142,67 +143,108 @@ sub GetBitcoinPriceRating
 		$config->param("last_average", $next_average);
 	}
 	say "Average: $next_average";
-	my $difference = floor(100*($next_average - $last_average) + 0.5)/100;
-	say "Difference: $difference";
-	my $critical = 0;
-	# TODO: revamp the critical test - this logic seems bogus
-	if ($difference > 0.2) { $critical = 1; }
-	if ($difference < -0.2) { $critical = 1; }
-	if ($difference > 1.0) { $critical = 0.5; }
-	if ($difference < -1.0) { $critical = 0.5; }
-	if ($critical == 0 && $price > $next_average * 1.05) { $rating = 'Soft Sell'; }
-	if ($critical == 0 && $price > $next_average * 1.11) { $rating = 'Peak Sell'; }
-	if ($critical == 0.5 && $price > $next_average * 1.25) { $rating = 'Spiking Sell'; }
-	if ($critical == 0.5 && $next_average * 0.75 > $price) { $rating = 'Dropping Buy'; }
-	if ($critical == 0 && $next_average * 0.95 > $price) { $rating = 'Valley Buy'; }
-	say "Critical: $critical";
+	my $critical = GetCriticalRating($price, $last_average, $next_average);
+	$rating = GetPriceRating($price, $next_average, $critical, $rating);
+
+	my $advice = rangeFinder($rating, $next_average, $last_average, 0.95, 1.05, $rating);
+	say "Advice: $advice";
 
 	return $rating;
 }
 
+sub GetPriceRating
+{
+	my $price = shift;
+	my $ema = shift;
+	my $critical = shift;
+	my $rating = shift;
+
+	if ($critical == 0   && $price > $ema * 1.05) { $rating = 'Soft Sell'; }
+	if ($critical == 0   && $price > $ema * 1.11) { $rating = 'Peak Sell'; }
+	if ($critical == 0.5 && $price > $ema * 1.25) { $rating = 'Spiking Sell'; }
+	if ($critical == 0.5 && $ema * 0.75 > $price) { $rating = 'Dropping Buy'; }
+	if ($critical == 0   && $ema * 0.95 > $price) { $rating = 'Valley Buy'; }
+
+	return $rating;
+}
+
+sub GetCriticalRating
+{
+	my $price = shift;
+	my $previousEma = shift;
+	my $ema = shift;
+
+	my $difference = RoundToTwoDecimals($ema - $previousEma);
+
+	my $critical = 1;
+	if (abs($difference) < 0.2)
+	{
+		$critical = 0;
+	}
+	elsif (abs($difference) < 1.0)
+	{
+		$critical = 0.5;
+	}
+
+	return $critical;
+}
+
+sub GetRandomHoldMessage
+{
+	my @hold_messages = (
+		'Ho Ho Ho Hold!',
+	  	'HODLing like a boss',
+	  	'Just keep keep holding, just keep holding ... what do we do we HODL!',
+		'What are you doing, #Bitcoiner? I can\'t help you right now. (HODL)',
+		'History will record this day as just another day. (HODL)',
+		'HODL me baby, one more time.',
+		'I am required to issue a hold rating by my benevolent overlord.'
+	);
+	my $list_size = $#hold_messages;
+	my $luck = int(rand($list_size));
+
+	return $hold_messages[$luck];
+}
+
 sub rangeFinder
 {
-#	#TODO: I didn't like what this did during periods of relative stability
-#	my $advice = shift;
-#	my $baseline_rating = $advice;
-#	my $min_price = RoundToTwoDecimals($next_average * 0.95);
-#	my $max_price = RoundToTwoDecimals($next_average * 1.05);
-#	my $iteration = RoundToTwoDecimals(($max_price - $min_price) / 100);
-#	for (my $price = $min_price; $price <= $max_price; $price += $iteration)
-#	{
-#		$xp = floor($price * 100 + 0.5);
-#		$la = floor($last_average * 100 + 0.5);
-#		$next_average = floor(($la * 29 + $xp)/30 + 0.5) / 100;
-#		my $difference = floor(100*($next_average - $last_average) + 0.5)/100;
-#		my $critical = 0;
-#		if ($difference > 0.2) { $critical = 1; }
-#		if ($difference < -0.2) { $critical = 1; }
-#		if ($difference < 1.0 && $difference > 0.2) { $critical = 0.5; }
-#		if ($difference > -1.0 && $difference < -0.2) { $critical = 0.5; }
-#
-#		if ($critical == 0 && $price > $next_average * 0.9) { $rating = 'Soft Sell'; }
-#		if ($critical == 0 && $price > $next_average * 1.11) { $rating = 'Peak Sell'; }
-#		if ($critical == 0.5 && $price > $next_average * 1.2) { $rating = 'Spiking Sell'; }
-#		if ($critical == 0.5 && $next_average * 0.75 > $price) { $rating = 'Dropping Buy'; }
-#		if ($critical == 0 && $next_average * 0.85 > $price) { $rating = 'Valley Buy'; }
-#
-#		if ($bcrit == 0 && $critical == 0)
-#		{
-#			$bcrit = RoundToTwoDecimals($price);
-#			$advice = $rating;
-#		}
-#		if ($critical == 0)
-#		{
-#			$ecrit = RoundToTwoDecimals($price);
-#		}
-#		$rating = $hold_messages[$luck];
-#	}
-#
-#	if ($bcrit != 0 && $ecrit != $bcrit)
-#	{
-#		$advice .= ": $bcrit - $ecrit";
-#	}
-#	return $advice;
+	my $advice = shift;
+	my $next_average = shift;
+	my $last_average = shift;
+	my $lower_bound = shift;
+	my $upper_bound = shift;
+	my $rating = shift;
+
+	my $first_critical_value = 0;
+	my $last_critical_value = 0;
+	my $baseline_rating = $advice;
+	my $min_price = RoundToTwoDecimals($next_average * $lower_bound);
+	my $max_price = RoundToTwoDecimals($next_average * $upper_bound);
+	my $iteration = RoundToTwoDecimals(($max_price - $min_price) / 100);
+	for (my $price = $min_price; $price <= $max_price; $price += $iteration)
+	{
+		$next_average = CalculateNextEMA($last_average, $price, 60);
+
+		my $critical = GetCriticalRating($price, $last_average, $next_average);
+		$rating = GetPriceRating($price, $next_average, $critical, $rating);
+
+		if ($first_critical_value == 0 && $critical == 0)
+		{
+			$first_critical_value = RoundToTwoDecimals($price);
+			$advice = $rating;
+		}
+		if ($critical == 0)
+		{
+			$last_critical_value = RoundToTwoDecimals($price);
+		}
+		$rating = GetRandomHoldMessage();
+	}
+
+	if ($first_critical_value != 0 && $last_critical_value != $first_critical_value)
+	{
+		$advice .= ": $first_critical_value - $last_critical_value";
+	}
+	return $advice;
 }
 
 sub PostBitcoinRating
